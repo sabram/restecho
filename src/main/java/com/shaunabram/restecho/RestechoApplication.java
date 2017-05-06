ac89b5bb33cad9523a795e5c438608e73e531233
sabram <sabram@lendingclub.com>
2017-05-06 15:54:47 -0700

    Initial commit

---
+++ src/main/java/com/shaunabram/restecho/RestechoApplication.java
@@ -0,0 +1,12 @@
+package com.shaunabram.restecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestechoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shaunabram/restecho/RestechoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shaunabram.restecho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestechoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
